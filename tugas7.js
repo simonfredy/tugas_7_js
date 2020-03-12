c0cab82f57534d1bdff2d0b671313427bf16c393
Simon Fredy Sinaga <simonfredysinaga@gmail.com>
2020-03-12 19:16:52 +0700

    Tugas 7 JS

---
+++ tugas7.js
@@ -0,0 +1,14 @@
+function tugasTujuh() {
+    let nArray = [1, 2, 3, 4, 5, 6 ,7, 8, 9, 10];
+
+
+    for (i = 0; i < nArray.length; i++){
+        console.log(nArray[i]);
+    }
+    console.log("");
+    for (let i of nArray){
+        console.log(i);
+    }
+}
+
+tugasTujuh();
